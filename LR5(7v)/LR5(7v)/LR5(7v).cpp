ac459fc2a59e699109399e69db5c47ee7f615b61
MikhailGorbach <michagorbach57@gmail.com>
2021-09-11 11:36:43 +0300

    add >>, <<, remark for op +

--- LR5(7v)/LR5(7v)/LR5(7v).cpp
+++ LR5(7v)/LR5(7v)/LR5(7v).cpp
@@ -17,18 +17,20 @@ public:
 	void setB(int b);
 	void setC(int c);
 	int operator +();
+	//Triangle& operator +();  смотреть 118 строку
 	friend bool operator !(Triangle& other);
 	void operator <<(Triangle& other);
 	bool operator <=(Triangle& other);
 	friend bool operator ==(Triangle& other1, Triangle& other2);
+	friend istream& operator >>(istream& in, Triangle& other);
 };
 
 int main()
 {
 	cout << "Write sides of first triangle" << endl;
-	int a = 0, b = 0, c = 0; cin >> a >> b >> c;
-	
-	Triangle tr1(a,b,c);
+	Triangle tr1;
+	operator>>(cin, tr1);
+
 	if (operator!(tr1))
 	{
 		tr1.operator<<(tr1);
@@ -41,9 +43,9 @@ int main()
 	}
 
 	cout << "Write sides of second triangle" << endl;
-	a = 0, b = 0, c = 0; cin >> a >> b >> c;
-	
-	Triangle tr2(a, b, c);
+	Triangle tr2;
+	operator>>(cin, tr2);
+
 	if (operator!(tr2))
 	{
 		tr2.operator<<(tr2);
@@ -112,7 +114,13 @@ Triangle::Triangle(int a, int b, int c)
 	this->b = b;
 	this->c = c;
 }
-int Triangle::operator +()
+//Triangle& Triangle::operator +()
+//{
+//	Triangle temp;
+//	temp.setA(getA() + getB() + getC());
+//	return temp;
+//}
+int Triangle::operator+()
 {
 	int p = getA() + getB() + getC();
 	return p;
@@ -127,6 +135,7 @@ bool operator !(Triangle& other)
 }
 void Triangle::operator <<(Triangle& other)
 {
+	//cout << "Perimeter: " << other.operator+().getA() << endl;b
 	cout << "Perimeter: " << other.operator+() << endl;
 }
 bool Triangle::operator <=(Triangle& other)
@@ -141,4 +150,11 @@ bool operator ==(Triangle& other1, Triangle& other2)
 		(other1.b == other2.b) &&
 		(other1.c == other2.c)) return true;
 	return false;
+}
+istream& operator >>(istream& in, Triangle& other)
+{
+	in >> other.a;
+	in >> other.b;
+	in >> other.c;
+	return in;
 }

26f6721f6168303f7b46fa95c1b677a045b5c268
MikhailGorbach <michagorbach57@gmail.com>
2021-09-13 20:22:35 +0300

    basis for LR7

---
+++ LR7(7v)/LR7(7v)/LR7(7v).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Write 1-st number: ";
+	int a = 0;
+	cin >> a;
+	
+	cout << "Write 2-nd number: ";
+	int b = 0;
+	cin >> b;
+
+	cout << a / b << endl;
+	cout << a % b << endl;
+
+	system("pause");
+	return 0;
+}
